1771182599af82fc0778b90ef8b19d29d5db1049
rohnthomas <rohnthomas22@gmail.com>
2025-05-23 17:21:22 +0530

    completed the guess-game!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world, I started learning RUST...seems cool!");
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+fn main(){
+    let crct_numb:u32=rand::thread_rng().gen_range(1..=100);
+    
+    loop{
+        println!("please enter your guess");
+        let mut guess=String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to get line");
+
+        let guess:u32=match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>continue,
+        };
+        println!("you guessed {}",guess);
+        
+        match guess.cmp(&crct_numb){
+            Ordering::Less=>println!("too small"),
+            Ordering::Greater=>println!("too big"),
+            Ordering::Equal=>{
+                println!("yahooo! thats the correct one");
+                break;}
+
+        }
+
+    }
+
+
+
+ 
 }
